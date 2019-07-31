4ae7258cba4fac2f1445627918617c987c85711d
adhithyan <msadhithyan3@gmail.com>
2019-07-31 11:34:54 +0530

    remove unwanted importes

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -5,14 +5,11 @@
  */
 package com.example.demo.helloworld;
 
-import java.util.Locale;
 
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
